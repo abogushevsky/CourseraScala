9fce327d4d53386a979830a35916fef242aa6464
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-09-19 13:08:11 +0300

    recfun solved 2 of 3

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,12 +14,34 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = 
+    if (r == 0 || c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+      
+      def balance(chars: List[Char], openedP: Int): Int = {
+        def openedPInternal = openedP
+        if (chars.isEmpty) {
+          return openedPInternal
+        }
+        
+        if (chars.head == ')') {
+          if (openedPInternal < 1)
+            return 1
+          else
+            return balance(chars.tail, openedPInternal - 1)
+        } else if (chars.head == '(') 
+          return balance(chars.tail, openedPInternal + 1)
+        
+        return balance(chars.tail, openedPInternal)
+      }
+            
+      val openedCnt = balance(chars, 0)
+      openedCnt == 0
+  }
 
   /**
    * Exercise 3
